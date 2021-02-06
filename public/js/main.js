97f360c083b17a1aa9572b3759b4f3ff0f89b6ee
Roque Laecio dos Santos <rlaecio@hotmail.com>
2021-02-06 17:30:57 +0000

    botao-remover

--- public/js/main.js
+++ public/js/main.js
@@ -70,15 +70,23 @@ function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var numPalavras = $("#contador-palavras").text();
     var usuario = "Roque Laecio";
+    var botaoRemover = "<a href='#'> <i class='small material-icons'>delete</i></a>"
 
     var linha = "<tr>" +
                     "<td>" + usuario + "</td>" +
                     "<td>" + numPalavras + "</td>" +
+                    "<td>" + botaoRemover + "</td>" +
                 "</tr>";
 
     corpoTabela.prepend(linha);
 }
- 
+
+
+$(".botao-remover").click(function(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+});
+
 function reiniciaJogo() {
     campo.attr("disabled", false);
     campo.val("");

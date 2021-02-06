95396d968ece483f1e49acf87cd96f94c90009ad
Roque Laecio dos Santos <rlaecio@hotmail.com>
2021-02-06 19:03:11 +0000

    dividindo as responsabilidades

--- public/js/main.js
+++ public/js/main.js
@@ -66,46 +66,6 @@ function inicializaMarcadores() {
     });
 }
 
-function inserePlacar() {
-    var corpoTabela = $(".placar").find("tbody");
-    var numPalavras = $("#contador-palavras").text();
-    var usuario = "Roque Laecio";
-
-    var linha = novaLinha(usuario,numPalavras);
-    linha.find(".botao-remover").click(removeLinha);
-
-    corpoTabela.append(linha);
-    
-}
-
-function novaLinha(usuario,palavras){
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href","#");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    // Icone dentro do <a>
-    link.append(icone);
-
-    // <a> dentro do <td>
-    colunaRemover.append(link);
-
-    // Os três <td> dentro do <tr>
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-function removeLinha(event){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
-
 function reiniciaJogo() {
     campo.attr("disabled", false);
     campo.val("");

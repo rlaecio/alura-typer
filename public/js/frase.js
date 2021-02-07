e710b4227c487dbb5a48fc24214c81e5707a8792
Roque Laecio dos Santos <rlaecio@hotmail.com>
2021-02-07 00:55:43 +0000

    busca com get

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,5 +1,7 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
+// busca aleatoriamente todas as frase do servidor
 function fraseAleatoria() {
     $("#spinner").toggle();
 
@@ -12,10 +14,37 @@ function fraseAleatoria() {
     })
     .always(function () {
         $("#spinner").toggle();
-      })
+    })
+}
+
+// busca frase especifica com GET
+function buscaFrase() {
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    var dados = { id: fraseId};
+    
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle()
+        }, 2500);
+    })
+    .always(function () {
+        $("#spinner").toggle();
+    })
 }
 
 
+// efetua a troca da frase buscada especificamente
+function trocaFrase(data) {
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
+}
+
+// efetua a troca da frase buscada aleatoriamente
 function trocaFraseAleatoria(data) {
     var frase = $(".frase");
     var numeroAleatorio = Math.floor(Math.random() * data.length);
